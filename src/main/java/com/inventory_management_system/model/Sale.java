ebd2dcdf7f47dc8061568dd52781637941dd6795
odyshkantiuk <odyshkantiuk@gmail.com>
2023-03-28 17:39:28 +0300

    update

---
+++ src/main/java/com/inventory_management_system/model/Sale.java
@@ -0,0 +1,67 @@
+package com.inventory_management_system.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Sale {
+    private int id;
+    private Date date;
+    private Customer customer;
+    private List<Product> products;
+
+    public Sale(int id, Date date, Customer customer, List<Product> products) {
+        this.id = id;
+        this.date = date;
+        this.customer = customer;
+        this.products = products;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        this.products.remove(product);
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+}
